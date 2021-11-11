9bc46bf45930b8cf686eb6685389fe5fb0ea55ed
JBe <jmbeaderstadt@gmail.com>
2021-11-11 14:04:35 -0600

    Added combatants and the Combatant Track.

---
+++ RollForInitiative/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RollForInitiative.Data
+{
+    public class AppDBContext
+    {
+    }
+}
---
+++ RollForInitiative/Data/Combatant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RollForInitiative.Data
+{
+    public class Combatant
+    {
+        public string Name { get; private set; }
+        public int HitPointsCurrent { get; private set; }
+        public int HitPointsMax { get; private set; }
+        public int InitiativeBonus { get; private set; }
+
+        public float InitiativeCurrent { get; private set; }
+        public int ArmorClass { get; private set; }
+
+        public Combatant(string name, int hitPointsCurrent, int hitPointsMax, int initiativeBonus, int armorClass)
+        {
+            Name = name;
+            HitPointsCurrent = hitPointsCurrent;
+            HitPointsMax = hitPointsMax;
+            InitiativeBonus = initiativeBonus;
+            ArmorClass = armorClass;
+        }
+    }
+}
